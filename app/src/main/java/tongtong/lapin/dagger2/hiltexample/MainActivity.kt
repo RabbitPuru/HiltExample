0dd736913bc73a374d0f7101fc0bba601b80a953
tongtonglapin <kimsh3933@gmail.com>
2022-01-12 19:02:46 +0900

    first commit

---
+++ app/src/main/java/tongtong/lapin/dagger2/hiltexample/MainActivity.kt
@@ -0,0 +1,11 @@
+package tongtong.lapin.dagger2.hiltexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
